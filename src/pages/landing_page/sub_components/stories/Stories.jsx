2220a3e2a2b34109edb00e92ae181c5dd29bcbe6
sajinskumar132 <sajinskumar132@gmail.com>
2025-03-02 01:16:24 +0530

    Updated

--- src/pages/landing_page/sub_components/stories/Stories.jsx
+++ src/pages/landing_page/sub_components/stories/Stories.jsx
@@ -1,40 +1,26 @@
 import React from "react";
-import './storiesStyle.css'
+import "./storiesStyle.css";
+import { stories_helper } from "./stories_helper";
 function Stories() {
   return (
     <div className="stories_main_container">
       <p className="stories_headline">Stories of Strength & Hope</p>
-      <p className="stories_sub_headline">Real people. Real struggles. Real comebacks.</p>
+      <p className="stories_sub_headline">
+        Real people. Real struggles. Real comebacks.
+      </p>
       <div className="stories_video_container">
-        <iframe
-           className="stories_video_frame"
-        //   width="560"
-        //   height="315"
-          src="https://www.youtube.com/embed/UD0tnoDgC-8?si=qzVGlg_Spqe8x2mR"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe>
-        <iframe
-          className="stories_video_frame"
-          src="https://www.youtube.com/embed/3r1vz2ML-HI?si=EsrB1CB3TNu6PBQg"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe>
-        <iframe
-          className="stories_video_frame"
-          src="https://www.youtube.com/embed/d96akWDnx0w?si=OonKJwz4r-e01K-t"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe>
+        {stories_helper.map((item, index) => (
+          <iframe
+            key={index}
+            className="stories_video_frame"
+            src={item}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        ))}
       </div>
     </div>
   );
---
+++ src/pages/landing_page/sub_components/stories/stories_helper.js
@@ -0,0 +1,5 @@
+export const stories_helper=[
+     "https://www.youtube.com/embed/UD0tnoDgC-8?si=qzVGlg_Spqe8x2mR",
+     "https://www.youtube.com/embed/3r1vz2ML-HI?si=EsrB1CB3TNu6PBQg",
+     "https://www.youtube.com/embed/d96akWDnx0w?si=OonKJwz4r-e01K-t"
+]
