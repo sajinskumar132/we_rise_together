d87f827373265004997d04c5f4d017d21818e4ed
sajinskumar132 <sajinskumar132@gmail.com>
2025-03-11 06:15:36 +0530

    stories section latest changes

--- src/pages/landing_page/sub_components/stories/Stories.jsx
+++ src/pages/landing_page/sub_components/stories/Stories.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./storiesStyle.css";
 import { stories_helper } from "./stories_helper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+// import required modules
+import { FreeMode, Pagination } from 'swiper/modules';
 function Stories() {
   return (
     <div className="stories_main_container">
@@ -9,7 +17,30 @@ function Stories() {
         Real people. Real struggles. Real comebacks.
       </p>
       <div className="stories_video_container">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+        breakpoints={{
+          0:{ slidesPerView: 1, spaceBetween: 0 },
+          600: { slidesPerView: 2, spaceBetween: 10 },
+          768: { slidesPerView: 2, spaceBetween: 25 },
+          1024: { slidesPerView: 3, spaceBetween: 50 },
+        }}
+      navigation={{
+        nextEl:true,
+        prevEl:true
+      }}
+      
+      >
         {stories_helper.map((item, index) => (
+          <SwiperSlide id={index}>
           <iframe
             key={index}
             className="stories_video_frame"
@@ -20,7 +51,10 @@ function Stories() {
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
           ></iframe>
+          </SwiperSlide>
         ))}
+      </Swiper>
+        
       </div>
     </div>
   );
